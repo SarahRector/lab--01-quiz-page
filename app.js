54354ad6ae8bfb1d635dc746d68113e7e42fac2b
SarahRector <rector.sarah.r@gmail.com>
2020-07-06 16:35:04 -0700

    imported and exported function, scored results

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+
+
+
 // import functions and grab DOM elements
+import { countsAsAYes } from './utils.js';
 const quizButton = document.getElementById('quiz-button');
 
 // initialize state
@@ -11,10 +15,36 @@ quizButton.addEventListener('click', () => {
     const lastName = prompt("What's your last name?");
     name = name + ' ' + lastName;
 
+    const really = confirm("Do you really want to test your derby knowledge?");
+    if (!really) return;
+
     const derbyHistory = prompt("Are the Wheels of Justice ranked number one in the world? (yes or no)");
 
     const howToPlay = prompt("Can the pivot become the jammer? (yes or no)");
 
     const gnarWallsFav = prompt("Does Braun Swanson have a sibling named Bald Eagle? (yes or no)");
-    console.log(gnarWallsFav);
+    
+    let score = 0;
+    if (countsAsAYes(derbyHistory)) {
+        score++;
+    };
+        
+    if (countsAsAYes(howToPlay)) {
+        score++;
+    };
+        
+    if (!countsAsAYes(gnarWallsFav)) {
+        score++;
+    };
+
+    let response = "Congratualtions, " + name + "!";
+
+    if (score >= 1) {
+        response += " You're ready to roll!";
+    } else {
+        response += " You're headed to the penalty box! Read up and take the quiz again!";
+    }
+    quizButton.textContent = response;
+    
+    console.log(score);
 });
