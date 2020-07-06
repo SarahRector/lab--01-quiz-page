67f53a5c0de13ce5d204b0f9504f3f8fd8e1bc7f
SarahRector <rector.sarah.r@gmail.com>
2020-07-06 15:31:23 -0700

    added event listener and questions

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 // import functions and grab DOM elements
+const quizButton = document.getElementById('quiz-button');
 
 // initialize state
 
 // set event listeners to update state and DOM
+quizButton.addEventListener('click', () => {
+    const firstName = prompt("What's your first name?");
+    name = firstName;
+
+    const lastName = prompt("What's your last name?");
+    name = name + ' ' + lastName;
+
+    const derbyHistory = prompt("Are the Wheels of Justice ranked number one in the world? (yes or no)");
+
+    const howToPlay = prompt("Can the pivot become the jammer? (yes or no)");
+
+    const gnarWallsFav = prompt("Does Braun Swanson have a sibling named Bald Eagle? (yes or no)");
+    console.log(gnarWallsFav);
+});
