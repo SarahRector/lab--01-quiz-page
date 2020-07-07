87862fb121125521fd6d9db36e493f8e0f36ffd3
SarahRector <rector.sarah.r@gmail.com>
2020-07-06 17:59:55 -0700

    added tests and styling

--- app.js
+++ app.js
@@ -10,34 +10,34 @@ const quizButton = document.getElementById('quiz-button');
 // set event listeners to update state and DOM
 quizButton.addEventListener('click', () => {
     const firstName = prompt("What's your first name?");
-    name = firstName;
+    let name = firstName;
 
     const lastName = prompt("What's your last name?");
-    name = name + ' ' + lastName;
+    let fullName = name + ' ' + lastName;
 
-    const really = confirm("Do you really want to test your derby knowledge?");
+    const really = confirm('Do you really want to test your derby knowledge?');
     if (!really) return;
 
-    const derbyHistory = prompt("Are the Wheels of Justice ranked number one in the world? (yes or no)");
+    const derbyHistory = prompt('Are the Wheels of Justice ranked number one in the world? (yes or no)');
 
-    const howToPlay = prompt("Can the pivot become the jammer? (yes or no)");
+    const howToPlay = prompt('Can the pivot become the jammer? (yes or no)');
 
-    const gnarWallsFav = prompt("Does Braun Swanson have a sibling named Bald Eagle? (yes or no)");
+    const gnarWallsFav = prompt('Does Braun Swanson have a sibling named Bald Eagle? (yes or no)');
     
     let score = 0;
     if (countsAsAYes(derbyHistory)) {
         score++;
-    };
+    }
         
     if (countsAsAYes(howToPlay)) {
         score++;
-    };
+    }
         
     if (!countsAsAYes(gnarWallsFav)) {
         score++;
-    };
+    }
 
-    let response = "Congratualtions, " + name + "!";
+    let response = 'Congratualtions, ' + fullName + '!';
 
     if (score >= 1) {
         response += " You're ready to roll!";
@@ -46,5 +46,4 @@ quizButton.addEventListener('click', () => {
     }
     quizButton.textContent = response;
     
-    console.log(score);
 });
