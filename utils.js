818d6c35103f3cce8420c066ce1d73017640078f
Shawn Carpenter <shawn.carpeenter@gmail.com>
2020-07-09 16:46:14 -0700

    added some styling and refactored countsAsYes function, refactored response strings

--- utils.js
+++ utils.js
@@ -1,6 +1,5 @@
 export function countsAsAYes(word) {
     const firstLetter = word.charAt(0).toLowerCase();
-    const isAY = firstLetter === 'y';
-    return isAY;
+    return firstLetter === 'y';
 }
     
